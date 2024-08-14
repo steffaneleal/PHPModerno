20d528cd8e3377017a9c976dff49f4317790ee3a
Stéffane Leal <92050677+steffaneleal@users.noreply.github.com>
2024-08-14 14:28:24 -0300

    Update index.php

--- cursophp/desafios/desafio11-reajustadorDePrecos/index.php
+++ cursophp/desafios/desafio11-reajustadorDePrecos/index.php
@@ -11,24 +11,33 @@
         $preco = $_GET["preco"];
         $porcentagem = $_GET["porcentagem"];
     ?>
+
     <main>
         <h1>Reajustador de Preços</h1>
         <form action="<?= $_SERVER["PHP_SELF"] ?>" method="get">
             <label for="preco">Preço do Produto (R$)</label>
             <input type="number" name="preco" id="id_preco" value="0" step="0.01">
-            <label for="porcentagem">Qual será o percentual de reajuste? <strong>()</strong></label>
+            <label for="porcentagem">Qual será o percentual de reajuste? <strong><span id="p">?</span>%</strong></label>
+            <input type="range" name="porcentagem" id="id_porcentagem" min="0" max="100" step="1" oninput="mudaValor()">
             <!-- <input type="range" name="" id="" value=""> --> <!-- acho que preciso de javascript pra fazer isso, então vou usar o normal por enquanto, até ver a resolução do Guanabara -->
-            <input type="number" name="porcentagem" id="id_porcentagem" value="0">
+            <!-- <input type="number" name="porcentagem" id="id_porcentagem" value="0"> --> 
             <input type="submit" value="Reajustar">
         </form>
     </main>
+
     <section id="calcular">
-        <h1>Resultado do Reajuste</h1>
+        <h2>Resultado do Reajuste</h2>
         <?php 
             $preco_inicial = $preco;
             $reajuste = $preco +($preco * ($porcentagem / 100));
             echo "<p>O produto que custava R\$" . number_format($preco_inicial, 2, ",", ".") . ", com <strong>" . $porcentagem . "% de aumento </strong> vai passar a custar <strong> R\$" . number_format($reajuste, 2, ",", ".") . " </strong> a partir de agora.</p>";
         ?>
     </section>
+    
+    <script>
+        function mudaValor(){
+            p.innerText = id_porcentagem.value; //o texto dentro do elemento identificado como p vai ser 
+        }
+    </script>
 </body>
 </html>
