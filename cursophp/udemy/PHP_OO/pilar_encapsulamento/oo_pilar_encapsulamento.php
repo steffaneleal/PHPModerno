bb00d1a0d5ddb69772e083f3bc783f351aa9dbdd
Stéffane Leal <92050677+steffaneleal@users.noreply.github.com>
2024-09-16 15:03:37 -0300

    Update oo_pilar_encapsulamento.php
    - Encapsulamento

--- cursophp/udemy/PHP_OO/pilar_encapsulamento/oo_pilar_encapsulamento.php
+++ cursophp/udemy/PHP_OO/pilar_encapsulamento/oo_pilar_encapsulamento.php
@@ -8,16 +8,70 @@
         public $humor = 'Feliz';
 
         //métodos
-        public function getNome()
+        public function __get($atributo)
         {
-            return $this->nome;
+            return $this->$atributo;
         }
+
+        public function __set($atributo, $valor)
+        {
+            $this->$atributo = $valor;
+        }
+
+        private function executarMania()
+        {
+            echo "Assobiar";
+        }
+
+        protected function responder()
+        {
+            echo "Oi";
+        }
+
+        public function executarAcao()
+        {
+            $num = rand(1, 10); //número aleatório de 1 a 10
+
+            if($num >= 1 && $num <= 8)
+            {
+                $this->executarMania();
+            } else {
+                $this->responder();
+            }
+        }
+
+        //métodos simples
+        //PRIVATE
+        // public function getNome()
+        // {
+        //     return $this->nome;
+        // }
+
+        // public function setNome($value)
+        // {
+        //     if(strlen($value) >= 3)
+        //     {
+        //         $this->nome = $value;
+        //     }
+        // }
+
+
+        // //PROTECTED
+        // public function getSobrenome()
+        // {
+        //     return $this->sobrenome;
+        // }
+
+        // public function setSobrenome($value)
+        // {
+        //     $this->sobrenome = $value;
+        // }
     }
 
     $pai = new Pai();
-    echo $pai->getNome();
-    // $pai->humor = "Triste";
+    echo $pai->executarAcao();
+    // echo $pai->nome;
+    // echo $pai->sobrenome;
     // echo "<br>";
-    // echo $pai->humor;
-
+    // echo $pai->sobrenome = 'Oliveira';
 ?>
